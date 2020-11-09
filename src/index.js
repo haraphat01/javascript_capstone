51bbf94939c3698a8818a9f4165d569b2fe98fb4
haraphat01 <haraphat@gmail.com>
2020-11-09 10:22:46 +0100

    Finished setting up my environment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { scene } from './scenes/scene';
 const gameConfig = {
   width: 680,
   height: 400,
-  scene: scene
+  scene,
 };
 
 new Phaser.Game(gameConfig);
--- src/scenes/scene.js
+++ src/scenes/scene.js
@@ -1,5 +1,11 @@
-export class SimpleScene extends Phaser.Scene {
-    create() {
-      this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    }
+
+export class scene extends Phaser.Scene {
+  preload() {
+    this.load.image('cokecan', 'assets/cokecan.png');
   }
+
+  create() {
+    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+    this.add.image(100, 200, 'cokecan');
+  }
+}
